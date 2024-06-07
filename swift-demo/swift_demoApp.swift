17bd0c8e3a937abb8b101beac0b9a8581e9dba7d
Ariel Tyson <ariel30190@hotmail.com>
2024-06-06 19:14:40 -0700

    Initial Commit

---
+++ swift-demo/swift_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_demoApp.swift
+//  swift-demo
+//
+//  Created by Ariel Tyson on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
